ca1fe6e47b5368b6593ce8a1598857a7214d8fc6
forsigner <forsigner@gmail.com>
2023-04-10 17:30:06 +0800

    refactor: rm no use icons

--- src/icons/IconFeishu.tsx
+++
@@ -1,3 +0,0 @@
-import { Box } from '@fower/react'
-
-export const IconFeishu = () => <Box as="img" src="/images/feishu.png" square6 />
--- src/icons/IconSlash.tsx
+++
@@ -1,8 +0,0 @@
-import { iconify } from '@bone-ui/iconify'
-
-export const IconSlash = iconify({
-  displayName: 'IconSlash',
-  viewBox: '0 0 9 18',
-  fill: 'currentColor',
-  d: 'M1.032 16.825l6-16 .936.35-6 16-.936-.35z',
-})
